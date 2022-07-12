ec1306cad8065c74c5e4e9590b3b3918ce46e06a
Pedro de Mesquita Bittencourt <djojoo@gmail.com>
2022-07-12 14:04:56 -0300

    add and configure H2 env

--- src/main/java/com/attornatus/desafio/entities/Pessoa.java
+++ src/main/java/com/attornatus/desafio/entities/Pessoa.java
@@ -6,8 +6,17 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pessoa implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private Date dataDeNascimento;
